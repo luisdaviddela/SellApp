c8caa2e84712c0681516d1c55d534b72e1c971bc
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-11-29 17:28:02 -0600

    SplashScreen Hamburguer Menu

---
+++ BotanitApp/BotanitApp/BotanitApp/Views/MenuView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BotanitApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuView : MasterDetailPage
+	{
+		public MenuView ()
+		{
+			InitializeComponent ();
+            MyMenu();
+        }
+        public void MyMenu()
+        {
+            Detail = new NavigationPage(new InicioView());
+            List<Menu> menu = new List<Menu>
+            {
+                new Menu{ Page= new LoginPage(),MenuTitle="Mi Perfil",  MenuDetail="Mi perfil",icon="botanita.jpg"},
+            };
+            ListMenu.ItemsSource = menu;
+        }
+        private void ListMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var menu = e.SelectedItem as Menu;
+            if (menu != null)
+            {
+                IsPresented = false;
+                Detail = new NavigationPage(menu.Page);
+            }
+        }
+        public class Menu
+        {
+            public string MenuTitle
+            {
+                get;
+                set;
+            }
+            public string MenuDetail
+            {
+                get;
+                set;
+            }
+
+            public ImageSource icon
+            {
+                get;
+                set;
+            }
+
+            public Page Page
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
